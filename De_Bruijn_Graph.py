3e426387ff78705e5181442337b8dfb82615f507
shohamalh <49936945+shohamalh@users.noreply.github.com>
2020-11-29 19:12:27 +0200

    try to make exchange

--- De_Bruijn_Graph.py
+++ De_Bruijn_Graph.py
@@ -8,16 +8,22 @@ from PIL import Image
 class DeBruijnGraph:
     def __init__(self, seq, n):
         self.n = n
-        self.N = pow(2,n)
+        self.N = pow(2, n)
         self.G = nx.DiGraph()
         for i in range(len(seq) - n + 1):
-            self.G.add_edge(seq[i:i + n - 1], seq[i + 1:i + n]) #adds nodes and edges
+            self.G.add_edge(seq[i:i + n - 1], seq[i + 1:i + n], id_b=seq[i:i + n])  # adds nodes and edges
 
     def print_graph(self):
         self.G.graph['edge'] = {'arrowsize': '0.6', 'splines': 'curved'}
         self.G.graph['graph'] = {'scale': '3'}
-
         nx.nx_pydot.write_dot(self.G, 'graph.dot')
+
+        # graph_pos = nx.shell_layout(self.G)
+        # nx.draw_networkx_nodes(self.G, graph_pos)
+        # nx.draw_networkx_edges(self.G, graph_pos)
+        # nx.draw_networkx_labels(self.G, graph_pos)
+        # nx.draw_networkx_edge_labels(self.G, graph_pos, edge_labels='id_b')
+
         os.system('dot -Tpng graph.dot > De-Bruijn_graph.png')
         graph = Image.open('De-Bruijn_graph.png')
         graph.show()
@@ -29,12 +35,23 @@ class DeBruijnGraph:
     """
 
     # i is an n-1 bit input
-    def exchange(self, i) -> nx.DiGraph():
-        x = int(i, 2)  # i
-        y = x + 1  # i + N/2
-        z = x * 2  # 2i
-        w = x * 2 + 1  # 2i +1
-
+    def exchange(self, i):
+        n = self.n
+        x = int(i, 2) % n  # i
+        # y = x + 1  # i + N/2
+        z = x * 2 % n  # 2i
+        w = x * 2 + 1 % n  # 2i + 1
+        z1 = (z * 2) % n  # 4i
+        z2 = (z * 2 + 1) % n  # 41 + 1
+        w1 = (w * 2) % n  # 4i + 2
+        w2 = (w * 2 + 1) % n  # 41 + 3
+        # first_edge_to_change_with_prefix = bin(z) + '0'  #0bNUMBER
+        # first_edge_to_change = first_edge_to_change_with_prefix[2:]   #NUMBER
+        self.G.remove_edge(bin(z)[2:], bin(z1)[2:])  # remove 2i -> 4i
+        self.G.remove_edge(bin(w)[2:], bin(w1)[2:])  # remove 2i+1 -> 4i+2
+        self.print_graph()
+        self.G.add_edge(bin(z)[2:], bin(w1)[2:])  # add edge 2i -> 4i +2
+        self.G.add_edge(bin(w)[2:], bin(z1)[2:])  # add edge 2i+1 -> 4i
         # need to find the correct nodes and edges and then remove the edges and creates new ones in a new graph
 
     def __str__(self):
@@ -75,8 +92,6 @@ if __name__ == '__main__':
         sequence = '000001000110010100111010110111110000'
 
     G = DeBruijnGraph(sequence, n)
-    G.print_graph()
-    exit(0)
     G.exchange('10')
-    G.visualize(sequence, n)
+    G.print_graph()
     print(G)
